4c4f4238938385dc6846ea2113b69d01ed957051
Kryvyi Nazarii <kryvyynazariy@gamil.com>
2021-12-24 12:22:41 +0200

    Add remove duplicates from array task

---
+++ src/Array/RemoveDuplicatesFromSortedArray /removeDuplicates.test.ts
@@ -0,0 +1,20 @@
+import { twoSum } from "../Two Sum";
+import { removeDuplicates } from "./index";
+
+describe.skip.each([
+  { array: [1, 1, 2], expectedArray: [1, 2], result: 2 },
+  {
+    array: [0, 0, 1, 1, 1, 2, 2, 3, 3, 4],
+    expectedArray: [0, 1, 2, 3, 4],
+    result: 5,
+  },
+])("Remove duplicates from $array ", ({ array, expectedArray, result }) => {
+  test(`result should have indexes ${result}`, () => {
+    const k = removeDuplicates(array); // Calls your implementation
+
+    expect(k).toBe(expectedArray.length);
+    for (let i = 0; i < k; i++) {
+      expect(array[i]).toBe(expectedArray[i]);
+    }
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { twoSum } from "./Array/Two Sum";
+import {removeDuplicates} from "./Array/RemoveDuplicatesFromSortedArray ";
 
 twoSum([2, 7, 11, 15], 9)
+removeDuplicates( [0,0,1,1,1,2,2,3,3,4])
